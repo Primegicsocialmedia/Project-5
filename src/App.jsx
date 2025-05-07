606f0dc87d47f14b6fc0fee1c342153ea3de8b04
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-05-07 12:38:40 +0200

    Innitial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items center'>
+      <h1 className="text-5xl font-bold">Start your Project </h1>
+      
+    </div>
+  )
+}
+
+export default App
