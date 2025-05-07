3af6cfe7661066bd5c23d949f47d6947095374fc
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-05-07 13:14:54 +0200

    Background added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import BG from './components/BG'
 
 const App = () => {
   return (
-    <div className='flex justify-center items center'>
+    <div className='centerChild h-screen'>
+      <BG />
       <h1 className="text-5xl font-bold">Start your Project </h1>
       
     </div>
